88d5ff70213bf4d7f7b57bf2cfc6444f77de7db2
Dmitry R <dimsog@gmail.com>
2022-01-10 21:02:00 +0300

    Инициализация плагина блога

---
+++ plugins/dimsog/blog/Plugin.php
@@ -0,0 +1,96 @@
+<?php namespace Dimsog\Blog;
+
+use Backend;
+use System\Classes\PluginBase;
+
+/**
+ * Blog Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'Blog',
+            'description' => 'No description provided yet...',
+            'author'      => 'Dimsog',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+    /**
+     * Register method, called when the plugin is first registered.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Boot method, called right before the request route.
+     *
+     * @return array
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Registers any front-end components implemented in this plugin.
+     *
+     * @return array
+     */
+    public function registerComponents()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'Dimsog\Blog\Components\MyComponent' => 'myComponent',
+        ];
+    }
+
+    /**
+     * Registers any back-end permissions used by this plugin.
+     *
+     * @return array
+     */
+    public function registerPermissions()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'dimsog.blog.some_permission' => [
+                'tab' => 'Blog',
+                'label' => 'Some permission'
+            ],
+        ];
+    }
+
+    /**
+     * Registers back-end navigation items for this plugin.
+     *
+     * @return array
+     */
+    public function registerNavigation()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'blog' => [
+                'label'       => 'Blog',
+                'url'         => Backend::url('dimsog/blog/mycontroller'),
+                'icon'        => 'icon-leaf',
+                'permissions' => ['dimsog.blog.*'],
+                'order'       => 500,
+            ],
+        ];
+    }
+}
