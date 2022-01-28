dd9c4ac8d8b9f0455f27fffc011a2e04058c25a9
Dmitry R <dimsog@gmail.com>
2022-01-28 21:17:23 +0300

    Заготовка для компонента просмотра тега

---
+++ plugins/dimsog/blog/components/TagView.php
@@ -0,0 +1,19 @@
+<?php namespace Dimsog\Blog\Components;
+
+use Cms\Classes\ComponentBase;
+
+class TagView extends ComponentBase
+{
+    public function componentDetails()
+    {
+        return [
+            'name'        => 'TagView Component',
+            'description' => 'No description provided yet...'
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return [];
+    }
+}
