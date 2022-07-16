a2adc51399f6e0e3eb5d8f28a232071c148d0559
Dmitry R <dimsog@gmail.com>
2022-07-16 19:28:49 +0300

    Language improvements

---
+++ lang/en/lang.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'settings' => [
+        'site_name_label' => 'Site name',
+        'site_description_label' => 'Description',
+        'site_description_default' => 'Description for your blog',
+        'header_image_label' => 'Header image',
+        'logo_color_label' => 'Logo color',
+        'description_color_label' => 'Description color',
+        'menu_color_label' => 'Menu color',
+        'menu_color_hover_label' => 'Menu color (hover)',
+        'footer_html_block_label' => 'Footer HTML block',
+        'header_seo_block_label' => 'Header SEO block',
+        'footer_seo_block_label' => 'Footer SEO block',
+        'html_block_after_post_view_label' => 'Html block after post'
+    ]
+];
---
+++ lang/ru/lang.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'settings' => [
+        'site_name_label' => 'Название сайта',
+        'site_description_label' => 'Описание',
+        'site_description_default' => 'Описание вашего блога',
+        'header_image_label' => 'Изображение в шапке сайта',
+        'logo_color_label' => 'Цвет логотипа',
+        'description_color_label' => 'Цвет описания',
+        'menu_color_label' => 'Цвет пунктов меню',
+        'menu_color_hover_label' => 'Цвет пунктов меню при наведении',
+        'footer_html_block_label' => 'HTML блок в футере',
+        'header_seo_block_label' => 'SEO блок в шапке',
+        'footer_seo_block_label' => 'SEO блок в футере',
+        'html_block_after_post_view_label' => 'HTML блок после поста'
+    ]
+];
