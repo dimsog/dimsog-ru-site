19cbd4c8edb933613ca1c5120b246e653c4b911d
Dmitry R <dimsog@gmail.com>
2022-12-30 11:15:27 +0300

    feat: move settings from theme to the blog plugin

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,18 +1,5 @@
 <?php
 
 return [
-    'settings' => [
-        'site_name_label' => 'Site name',
-        'site_description_label' => 'Description',
-        'header_image_label' => 'Header image',
-        'logo_color_label' => 'Logo color',
-        'description_color_label' => 'Description color',
-        'menu_color_label' => 'Menu color',
-        'menu_color_hover_label' => 'Menu color (hover)',
-        'menu_color_active_label' => 'Menu color (active)',
-        'footer_html_block_label' => 'Footer HTML block',
-        'header_seo_block_label' => 'Header SEO block',
-        'footer_seo_block_label' => 'Footer SEO block',
-        'html_block_after_post_view_label' => 'Html block after post'
-    ]
+
 ];
